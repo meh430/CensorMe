2c38a1bf6630be158611b66a0803727b01a450f1
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-06 09:46:47 -0400

    Improves blocking

--- filter.js
+++ filter.js
@@ -1,4 +1,3 @@
-const elementNames = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "article", "section", "header", "span", "ul", "li", "div", "main"]; //, "UL", "LI", "OL"]
 let blockedWords = [];
 let blockedUrls = [];
 let filterOff = false;
@@ -10,32 +9,11 @@ const applyFilter = () => {
     }
 
     try {
-        chrome.storage.sync.get(["bUrls", "bWords"], (lists) => {
-            blockedUrls = lists.bUrls;
-            blockedWords = lists.bWords;
-            if (!isBlockedUrl()) {
-                console.log("blocked list: " + blockedWords);
-                elementNames.forEach(elName => {
-                    let elementsList = document.querySelectorAll(elName)
-                    elementsList.forEach(element => {
-                        blockedWords.forEach((word) => {
-                            element.innerHTML = replaceOccurrences(element.innerHTML, " " + word);
-                            element.innerHTML = replaceOccurrences(element.innerHTML, word + " ");
-                            element.innerHTML = replaceOccurrences(element.innerHTML, " " + word + " ");
-                        });                        
-                    })
-                })
-                /*for (let i = items.length; i--; ) {
-                    if (items[i].nodeName && elementNames.includes(items[i].nodeName)) {
-                        blockedWords.forEach((word) => {
-                            items[i].textContent = replaceOccurrences(items[i].textContent, word);
-                        });
-                    } else {
-                        continue;
-                    }
-                }*/
-            }
-        });
+        if (!isBlockedUrl()) {
+            console.log("blocked list: " + blockedWords);
+            censor(document.body);
+        }
+
         console.log("finished");
     } catch (err) {
         console.log(err);
@@ -47,17 +25,10 @@ const startTimer = () => {
 };
 
 chrome.storage.onChanged.addListener(() => {
-    chrome.storage.sync.get(["bWords"], (words) => {
-        blockedWords = words.bWords;
-        chrome.storage.sync.get(["bUrls"], (urls) => {
-            blockedUrls = urls.bUrls;
-            applyFilter();
-        });
-    });
-
-    chrome.storage.sync.get(["globalOff"], (filtOff) => {
-        console.log(filtOff.globalOff);
-        filterOff = filtOff.globalOff;
+    chrome.storage.sync.get(["globalOff", "bWords", "bUrls"], (info) => {
+        filterOff = info.globalOff;
+        blockedWords = info.bWords;
+        blockedUrls = info.bUrls;
         if (!filterOff) {
             startScript();
         }
@@ -70,28 +41,38 @@ chrome.storage.onChanged.addListener(() => {
 }*/
 
 function replaceOccurrences(text, oldValue) {
-    const pattern = oldValue.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+    const pattern = oldValue.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 
-    return text.replace(new RegExp(pattern, 'gi'), " BLOCKED ")
+    return text.replace(new RegExp(pattern, "gi"), "█".repeat(oldValue.length));
 }
 
-
+function censor(node) {
+    if (node.hasChildNodes()) {
+        node.childNodes.forEach(censor);
+    } else if (node.nodeType == Text.TEXT_NODE) {
+        if (blockedWords) {
+            blockedWords.forEach((word) => {
+                node.textContent = replaceOccurrences(node.textContent, word);
+            });
+        }
+    }
+}
 
 function startScript() {
-    chrome.storage.sync.get(["bUrls"], (urls) => {
-        blockedUrls = urls.bUrls;
-        if (isBlockedUrl()) {
-            console.log("whitelisted url");
-        } else {
-            window.addEventListener("load", applyFilter, false);
-            window.addEventListener("load", startTimer, false);
-        }
-    });
+    if (isBlockedUrl()) {
+        console.log("whitelisted url");
+    } else {
+        applyFilter();
+        startTimer();
+        //window.addEventListener("load", applyFilter, false);
+        //window.addEventListener("load", startTimer, false);
+    }
 }
 
-chrome.storage.sync.get(["globalOff"], (filtOff) => {
-    filterOff = filtOff.globalOff;
-    console.log(filtOff.globalOff);
+chrome.storage.sync.get(["globalOff", "bUrls", "bWords"], (info) => {
+    blockedUrls = info.bUrls;
+    blockedWords = info.bWords;
+    filterOff = info.globalOff;
     if (!filterOff) {
         startScript();
     }
