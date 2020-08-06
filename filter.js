922bda3c159b23144a8721a6fd6ce8974c1aab8f
Mehul Pillai <mehulpillai30@gmail.com>
2020-08-05 20:28:21 -0400

    Changes filter method

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
-const elementNames = ["P", "H1", "H2", "H3", "H4", "H5", "H6"]; //, "UL", "LI", "OL"]
+const elementNames = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "article", "section", "header", "span", "ul", "li", "div", "main"]; //, "UL", "LI", "OL"]
 let blockedWords = [];
 let blockedUrls = [];
 let filterOff = false;
@@ -10,23 +10,30 @@ const applyFilter = () => {
     }
 
     try {
-        chrome.storage.sync.get(["bUrls"], (urls) => {
-            blockedUrls = urls.bUrls;
+        chrome.storage.sync.get(["bUrls", "bWords"], (lists) => {
+            blockedUrls = lists.bUrls;
+            blockedWords = lists.bWords;
             if (!isBlockedUrl()) {
-                chrome.storage.sync.get(["bWords"], (words) => {
-                    blockedWords = words.bWords;
-                    console.log("blocked list: " + blockedWords);
-                    let items = document.getElementsByTagName("*");
-                    for (let i = items.length; i--; ) {
-                        if (items[i].nodeName && elementNames.includes(items[i].nodeName)) {
-                            blockedWords.forEach((word) => {
-                                items[i].textContent = replaceOccurrences(items[i].textContent, word);
-                            });
-                        } else {
-                            continue;
-                        }
+                console.log("blocked list: " + blockedWords);
+                elementNames.forEach(elName => {
+                    let elementsList = document.querySelectorAll(elName)
+                    elementsList.forEach(element => {
+                        blockedWords.forEach((word) => {
+                            element.innerHTML = replaceOccurrences(element.innerHTML, " " + word);
+                            element.innerHTML = replaceOccurrences(element.innerHTML, word + " ");
+                            element.innerHTML = replaceOccurrences(element.innerHTML, " " + word + " ");
+                        });                        
+                    })
+                })
+                /*for (let i = items.length; i--; ) {
+                    if (items[i].nodeName && elementNames.includes(items[i].nodeName)) {
+                        blockedWords.forEach((word) => {
+                            items[i].textContent = replaceOccurrences(items[i].textContent, word);
+                        });
+                    } else {
+                        continue;
                     }
-                });
+                }*/
             }
         });
         console.log("finished");
@@ -57,10 +64,18 @@ chrome.storage.onChanged.addListener(() => {
     });
 });
 
-function replaceOccurrences(text, oldStr) {
-    const tempReg = new RegExp(oldStr, "gi");
-    return text.replace(tempReg, "BLOCKED");
+/*function replaceOccurrences(text, oldStr) {
+    const reg = new RegExp(oldStr, "gi");
+    return text.replace(reg, "BLOCKED");
+}*/
+
+function replaceOccurrences(text, oldValue) {
+    const pattern = oldValue.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+
+    return text.replace(new RegExp(pattern, 'gi'), " BLOCKED ")
 }
+
+
 
 function startScript() {
     chrome.storage.sync.get(["bUrls"], (urls) => {
@@ -83,7 +98,7 @@ chrome.storage.sync.get(["globalOff"], (filtOff) => {
 });
 
 function isBlockedUrl() {
-    console.log(blockedUrls)
+    console.log(blockedUrls);
     const currUrl = window.location.origin;
     console.log(currUrl);
     let blocked = false;
